4f87c7ddef4d6c2eb6d5862b5c3dbb599cd1c2f8
Dima Pesotskiy <d.pesotzky@yandex.by>
2024-09-04 11:58:49 +0300

    save changes

--- book-store/src/pages/Account/SignUp.js
+++ book-store/src/pages/Account/SignUp.js
@@ -1,14 +1,4 @@
-import { useState } from "react"
 
-const [ name, setName ] = useState('');
-const [ mail, setNMail ] = useState('');
-const [ pass, setPass ] = useState('');
-
-const handleClickAuth = (e) => {
-    if (e.target) {
-
-    }
-}
 
 function SignUp () {
 
@@ -18,32 +8,32 @@ function SignUp () {
             <label for="text">
                 Name
                 <input
-                    placeholder="Your pname" value={value} onChange={e => setValue(e.target.value)} 
+                    placeholder="Your pname" 
                     type="text" name="password" id="password"
                 />
             </label>
             <label for="email">
                 Email
                 <input 
-                    placeholder="Your email" value={value} onChange={e => setValue(e.target.value)} 
+                    placeholder="Your email" 
                     type="email" name="email" id="email"
                 />
             </label>
             <label for="password">
                 Password
                 <input 
-                    placeholder="Your password" value={value} onChange={e => setValue(e.target.value)}
+                    placeholder="Your password" 
                     type="password" name="password" id="password"
                 />
             </label>
             <label for="password">
                 Confirm password
                 <input 
-                    placeholder="Confirm your password" value={value} onChange={e => setValue(e.target.value)} 
+                    placeholder="Confirm your password" 
                     type="password" name="password" id="password"
                 />
             </label>
-            <button onClick={handleClickAuth}>sign up</button>
+            <button >sign up</button>
         </div>
     )
 }
